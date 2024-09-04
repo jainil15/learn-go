103cccece9be2b1456ad55cb3c1d4d3f3ccfb398
Jainil <jainilpatel115@gmail.com>
2024-09-04 10:34:56 +0530

    Change: Formatting

--- types/user.go
+++ types/user.go
@@ -15,7 +15,6 @@ type User struct {
 	CreatedAt    time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
 }
-
 type RegisterUser struct {
 	FirstName    string `db:"first_name" json:"first_name" validate:"required"`
 	LastName     string `db:"last_name" json:"last_name"`
