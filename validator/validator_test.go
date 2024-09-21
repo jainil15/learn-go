9b496d81ee2ac875442a84ef59b25c1af8804546
Jainil <jainilpatel115@gmail.com>
2024-09-21 21:43:57 +0530

    Fix: Validation test

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -2,36 +2,42 @@ package validator
 
 import "testing"
 
-func ValidatiorTest(t *testing.T) {
-	// Test Required
-	if IsRequired("") != true {
-		t.Error("Required function failed")
-	}
-	if IsRequired("test") != false {
-		t.Error("Required function failed")
-	}
+func TestValidator(t *testing.T) {
+	t.Run("Testing Required", func(t *testing.T) {
+		if IsRequired("") != false {
+			t.Error("Required function failed")
+		}
+		if IsRequired("test") != true {
+			t.Error("Required function failed")
+		}
+	})
 
-	// Test MinLength
-	if IsMinLength("test", 5) != true {
-		t.Error("MinLength function failed")
-	}
-	if IsMinLength("test", 2) != false {
-		t.Error("MinLength function failed")
-	}
+	t.Run("Testing Min Length", func(t *testing.T) {
+		// Test MinLength
+		if IsMinLength("test", 5) != false {
+			t.Error("MinLength function failed")
+		}
+		if IsMinLength("test", 2) != true {
+			t.Error("MinLength function failed")
+		}
+	})
 
-	// Test MaxLength
-	if IsMaxLength("test", 2) != true {
-		t.Error("MaxLength function failed")
-	}
-	if IsMaxLength("test", 5) != false {
-		t.Error("MaxLength function failed")
-	}
+	t.Run("Testing Max Length", func(t *testing.T) {
+		// Test MaxLength
+		if IsMaxLength("test", 2) != false {
+			t.Error("MaxLength function failed")
+		}
+		if IsMaxLength("test", 5) != true {
+			t.Error("MaxLength function failed")
+		}
+	})
 
-	// Test Email
-	if IsEmail("test") != true {
-		t.Error("Email function failed")
-	}
-	if IsEmail("jainil@gmail.com") != false {
-		t.Error("Email function failed")
-	}
+	t.Run("Testing Max Length", func(t *testing.T) {
+		if IsEmail("test") != false {
+			t.Error("Email function failed")
+		}
+		if IsEmail("jainil@gmail.com") != true {
+			t.Error("Email function failed")
+		}
+	})
 }
