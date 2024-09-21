d32613997095c806ff466355c6e3c3b3ae708161
Jainil <jainilpatel115@gmail.com>
2024-09-21 21:33:33 +0530

    Fix: Validator test
    Validion testing fixed

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -4,34 +4,34 @@ import "testing"
 
 func ValidatiorTest(t *testing.T) {
 	// Test Required
-	if Required("") != true {
+	if IsRequired("") != true {
 		t.Error("Required function failed")
 	}
-	if Required("test") != false {
+	if IsRequired("test") != false {
 		t.Error("Required function failed")
 	}
 
 	// Test MinLength
-	if MinLength("test", 5) != true {
+	if IsMinLength("test", 5) != true {
 		t.Error("MinLength function failed")
 	}
-	if MinLength("test", 2) != false {
+	if IsMinLength("test", 2) != false {
 		t.Error("MinLength function failed")
 	}
 
 	// Test MaxLength
-	if MaxLength("test", 2) != true {
+	if IsMaxLength("test", 2) != true {
 		t.Error("MaxLength function failed")
 	}
-	if MaxLength("test", 5) != false {
+	if IsMaxLength("test", 5) != false {
 		t.Error("MaxLength function failed")
 	}
 
 	// Test Email
-	if Email("test") != true {
+	if IsEmail("test") != true {
 		t.Error("Email function failed")
 	}
-	if Email("jainil@gmail.com") != false {
+	if IsEmail("jainil@gmail.com") != false {
 		t.Error("Email function failed")
 	}
 }
